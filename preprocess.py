fe61a89c91b0550c85ed6366489ae26dd9bc5955
lucasb243 <lucas.brachuni@gmail.com>
2023-04-03 11:30:07 +0200

    cleanup

--- preprocess.py
+++ preprocess.py
@@ -16,44 +16,38 @@ if preprocess:
             'case:concept:name', 'activityNameEN', 'org:resource', 'time:timestamp',
             'case:last_phase', 'case:parts', 'action_code', 'r:municipality'
         ]]
-        
+        # Delete all rows were case_id = 4020737 --> Author response: "there is more than one value for attribute “ct:permit_type” "
         df = df[df['case:concept:name'] != 4020737]
-        
         df = df.rename(columns={
             # Resource-related
-            'municipality': 'r:municipality',
-            'case:concept:name' : 'Case ID',
-            'time:timestamp': 'Complete Timestamp',
-            "org:resource": "resource",
             "activityNameEN": "activity label",
             # CT-related
             'case:last_phase': 'ct:last_phase', 
             # AT-related
+            "action_code": "concept:name",
+            # Additional
+            'case:parts': 'case_parts',
         })
-        df = df.rename(columns={
-            'case:parts': 'case_parts'
-        })
-        
-        # TODO: derive 'ct:permit_type', 'at:phase'
+        # TODO: derive 'ct:permit_type', 'at:phase' --> specified by the author
         df = df[~df['case_parts'].isna()]
         df['ct:permit_type'] = df.apply(lambda row: 'Bouw' if 'Bouw' in str(row['case_parts']).split(',') else 'Non Bouw', axis=1)
 
         # only look at the main subprocess: "01_HOOFD"
-        df = df[~df['action_code'].isna()]
-        df = df[df['action_code'].str.startswith('01_HOOFD')]
-        df['at:phase'] = df['action_code'].apply(lambda code: code[:10])
+        df = df[~df['concept:name'].isna()]
+        df = df[df['concept:name'].str.startswith('01_HOOFD')]
+        df['at:phase'] = df['concept:name'].apply(lambda code: code[:10])
         
         # filter meaningless values
 
     # Universal (on Disco outputs)
     # derive and append TT related candidate attributes
-    df['Complete Timestamp'] = pd.to_datetime(df['Complete Timestamp'], format='%Y-%m-%d %H:%M:%S.%f')
+    df['time:timestamp'] = pd.to_datetime(df['time:timestamp'], format='%Y-%m-%d %H:%M:%S.%f')
     MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-    df['tt:month'] = df['Complete Timestamp'].apply(lambda ts: MONTHS[ts.month-1])
-    df['tt:day'] = df['Complete Timestamp'].apply(lambda ts: 'Day_{}'.format(ts.day))
+    df['tt:month'] = df['time:timestamp'].apply(lambda ts: MONTHS[ts.month-1])
+    df['tt:day'] = df['time:timestamp'].apply(lambda ts: 'Day_{}'.format(ts.day))
     WEEKDAYS = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-    df['tt:weekday'] = df['Complete Timestamp'].apply(lambda ts: WEEKDAYS[ts.dayofweek])
-    df['tt:ampm'] = df['Complete Timestamp'].apply(lambda ts: 'AM' if ts.hour < 12 else 'PM')
+    df['tt:weekday'] = df['time:timestamp'].apply(lambda ts: WEEKDAYS[ts.dayofweek])
+    df['tt:ampm'] = df['time:timestamp'].apply(lambda ts: 'AM' if ts.hour < 12 else 'PM')
     
     print(df)
     df.to_csv(f'data/processed/{log}.csv')
